43c564ebdffb019f8b7b99653abb3f4aa6aec4c6
Song Gao <song.gao.beta@gmail.com>
2012-03-03 14:25:31 -0600

    moved configuration.{cc,h} to project_1/src/

--- project_1/src/configuration.h
+++ project_1/src/configuration.h
@@ -1,9 +1,34 @@
-#ifndef CLASS_configuration
-#define CLASS_configuration
+#ifndef _conf_h
+#define _conf_h
+#include <map>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
 
-class configuration {
-    public:
-        configuration(std::string * config_file);
+struct gps {
+    unsigned int gps_x, gps_y, gps_z;
 };
 
+struct tuxname_port{
+    string tuxname;
+    int port;
+};
+ 
+class configuration{
+//fields
+    private:
+        map<int, vector<int> > * links;
+        map<int, struct gps > *gps_coordinate; 
+        map<int, struct tuxname_port> *name_port;
+
+    public:
+        configuration(string filename);
+        map<int, vector<int> > * getlinks();
+        map<int, struct gps> * get_gps_coordinate();
+        map<int, struct tuxname_port> * get_name_port();
+        ~configuration();
+};
+ 
 #endif
