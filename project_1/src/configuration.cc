42ce35bb553f99e558e5d25d187606ac7d625233
Song Gao <song.gao.beta@gmail.com>
2012-03-03 21:58:13 -0600

    updated configuration class gps data format

--- project_1/src/configuration.cc
+++ project_1/src/configuration.cc
@@ -10,9 +10,9 @@ using namespace std;
 
 configuration::configuration(string * filename){
     ifstream in(filename->c_str());
-    links = new map<int, vector<int> >;
-    gps_coordinate = new map<int, struct gps>;
-    name_port = new map<int, struct tuxname_port>;
+    links = new map<usint, vector<usint> >;
+    gps_coordinate = new map<usint, struct gps>;
+    name_port = new map<usint, struct tuxname_port>;
     if(!in){
         cout<<"cannot open file "<<filename<<endl;
     }
@@ -21,7 +21,7 @@ configuration::configuration(string * filename){
     int i = 0;
     struct tuxname_port tuxnode;
     struct gps gps_coord;
-    vector<int> linkednodes;
+    vector<usint> linkednodes;
     while(getline(in,line))
     {
         istringstream iss(line);
@@ -32,7 +32,7 @@ configuration::configuration(string * filename){
             tuxnode.tuxname = *iit;
             iit++;
             tuxnode.port = atoi((*iit).c_str());
-            name_port->insert(pair<int, struct tuxname_port>(i+1, tuxnode));
+            name_port->insert(pair<usint, struct tuxname_port>(i+1, tuxnode));
             iit++;//omit the word "links"
             iit++;
             linkednodes.clear();//make sure the vector is empty at first;
@@ -40,29 +40,29 @@ configuration::configuration(string * filename){
                 linkednodes.push_back(atoi((*iit).c_str()));
                 iit++;
             }
-            links->insert(pair<int, vector<int> >(i+1,linkednodes));
+            links->insert(pair<usint, vector<usint> >(i+1,linkednodes));
         }
         else{
-            gps_coord.gps_x = atoi((*iit).c_str());
+            gps_coord.gps_x = (float) atof((*iit).c_str());
             iit++;
-            gps_coord.gps_y = atoi((*iit).c_str());
+            gps_coord.gps_y = (float) atof((*iit).c_str());
             gps_coord.gps_z = 0; 
-            gps_coordinate->insert(pair<int, struct gps>(i-14,gps_coord));
+            gps_coordinate->insert(pair<usint, struct gps>(i-14,gps_coord));
         }
         i++;
     }
     in.close();
 }
 
-map<int, vector<int> > * configuration:: getlinks(){
+map<usint, vector<usint> > * configuration:: getlinks(){
     return links;
 }
 
-map<int, struct gps>* configuration:: get_gps_coordinate(){
+map<usint, struct gps>* configuration:: get_gps_coordinate(){
     return gps_coordinate;
 }
 
-map<int, struct tuxname_port> * configuration:: get_name_port() {
+map<usint, struct tuxname_port> * configuration:: get_name_port() {
     return name_port;
 }
 
