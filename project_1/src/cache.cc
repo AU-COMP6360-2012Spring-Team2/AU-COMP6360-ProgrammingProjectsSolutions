fc26e409a1c7d0bf2e52e7040493b8af53326c19
Song Gao <song.gao.beta@gmail.com>
2012-03-05 14:21:06 -0600

    fixed dead-lock in cache

--- project_1/src/cache.cc
+++ project_1/src/cache.cc
@@ -1,9 +1,12 @@
 #include "cache.h"
 
+#include <iostream>
+
 cache::cache(){
 }
 
 void cache::new_message(message * msg){
+    {
     lock_guard<mutex> lk_msg(this->_mutex_message_cache);
     lock_guard<mutex> lk_eebl(this->_mutex_eebl_cache);
 
@@ -19,6 +22,8 @@ void cache::new_message(message * msg){
         this->_eebl_cache_index.insert(pair<time_t, _type_eebl_hash_set::iterator>( time(NULL),
               this->_eebl_cache.insert(_type_vehicle_id_packet_id_pair(msg->originator_id(), msg->packet_id())).first
             ));
+    
+    }
 
     // remove outdated items.
     this->_remove_outdated_messages();
@@ -39,20 +44,21 @@ void cache::_remove_outdated_messages() {
 }
 
 void cache::_remove_outdated_eebl() {
-    lock_guard<mutex> lk(this->_mutex_message_cache);
+    lock_guard<mutex> lk(this->_mutex_eebl_cache);
 
     time_t now = time(NULL);
     auto i = this->_eebl_cache_index.begin();
-    for( ; cache::CACHED_EEBL_ID_LIVE_TIME < now - i->first; ++i)
+    for( ; cache::CACHED_EEBL_ID_LIVE_TIME < now - i->first && i != this->_eebl_cache_index.end(); ++i)
         this->_eebl_cache.erase(i->second);
     this->_eebl_cache_index.erase(this->_eebl_cache_index.begin(), i);
 }
 
 message * cache::get_latest_message_from(unsigned int vehicle_id){
-    lock_guard<mutex> lk(this->_mutex_message_cache);
 
     // remove outdated messages if necessary
     this->_remove_outdated_messages();
+
+    lock_guard<mutex> lk(this->_mutex_message_cache);
 
     // return NULL or pointer to required message
     auto r = this->_message_cache.find(vehicle_id);
@@ -63,10 +69,11 @@ message * cache::get_latest_message_from(unsigned int vehicle_id){
 }
 
 bool cache::has_recently_seen_eebl_of(unsigned int vehicle_id, unsigned int packet_id) {
-    lock_guard<mutex> lk(this->_mutex_eebl_cache);
 
     // remove outdated eebl if necessary
     this->_remove_outdated_eebl();
+
+    lock_guard<mutex> lk(this->_mutex_eebl_cache);
 
     return this->_eebl_cache.end() != this->_eebl_cache.find(_type_vehicle_id_packet_id_pair(vehicle_id, packet_id));
 }
